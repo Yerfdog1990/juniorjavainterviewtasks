1f8b18836e1f16ab95a8933dec9aa13c306cc62c
Yerfdog1990 <gouma308@gmail.com>
2024-07-27 05:43:52 +0300

    Create a singleton using a Private Constructor and a Public Static Final Field

---
+++ src/Task1/SingletonClassMethod1.java
@@ -0,0 +1,11 @@
+package Task1;
+
+/*
+Create a singleton using a Private Constructor and a Public Static Final Field:
+1.Declare a static final field in the class to hold the single instance.
+2.Mark the constructor private to prevent external instantiation.
+3.This approach ensures that only one instance of the class is created, and provides a simple way to access the instance.
+ */
+
+public class SingletonClassMethod1 {
+}
